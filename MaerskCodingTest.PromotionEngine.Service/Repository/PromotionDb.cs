bebc8157b792ca11730ff98c952e9eeb0eb86922
Nikhil <biragenikhil@gmail.com>
2020-09-12 19:26:27 +0530

    Ids has been fixed to ease the mannual testing while in swagger

--- MaerskCodingTest.PromotionEngine.Service/Repository/PromotionDb.cs
+++ MaerskCodingTest.PromotionEngine.Service/Repository/PromotionDb.cs
@@ -10,12 +10,12 @@ namespace MaerskCodingTest.PromotionEngine.Service.Repository
         {
             new PromotionType()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("c2ffb656-9d18-40ab-8074-97596f8ecadc"),
                 Name = "FixedPricePerNSKUItmes"
             },
             new PromotionType()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("10a9ad42-6884-4cf6-9529-babaed77ba4e"),
                 Name = "FixedPriceForMoreThanOneSKUs"
             }
         };
@@ -24,7 +24,7 @@ namespace MaerskCodingTest.PromotionEngine.Service.Repository
         {
             new Promotion()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("b2522dca-2522-46e4-ba83-70eb47effcfd"),
                 Name = "3 of A's for 130",
                 PrmotionTypeId = promotionTypes[0].Id,
                 FixedPrice = 130,
@@ -34,7 +34,7 @@ namespace MaerskCodingTest.PromotionEngine.Service.Repository
             },
              new Promotion()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("6286c124-30fd-46c8-8c9e-ac7d58ea38f6"),
                 Name = "2 of B's for 45",
                 PrmotionTypeId = promotionTypes[0].Id,
                 FixedPrice = 45,
@@ -44,7 +44,7 @@ namespace MaerskCodingTest.PromotionEngine.Service.Repository
             },
              new Promotion()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("d1e0c161-12db-4d3a-b00e-65a2c4ec8b07"),
                 Name = "C & D for 30",
                 PrmotionTypeId = promotionTypes[1].Id,
                 FixedPrice = 30,
